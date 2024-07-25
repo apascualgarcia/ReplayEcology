904c095c47f295f8afc2aafad5a1021c8f8ada5d
Matt Lloyd Jones <42059798+befriendabacterium@users.noreply.github.com>
2024-07-25 13:09:30 +0100

    add annotation and more save/load points in pipeline

--- src/dada2_convergence_pipeline_060522.R
+++ src/dada2_convergence_pipeline_060522.R
@@ -32,7 +32,9 @@ sample.names<-gsub("\\/", "_", sample.names)
 
 # INSPECT READ QUALITY PROFILES -------------------------------------------
 
-#qualplot_agg<-plotQualityProfile(file.path(path,fns), aggregate=T)
+#plot read quality profiles
+qualplot_agg<-plotQualityProfile(file.path(path,fns), aggregate=T)
+#save to 3_dada2 folder to save time later
 saveRDS(qualplot_agg,'4_dada2/qualplot_agg.RDS')
 
 # FILTER AND TRIM: PLACE FILTERED FILES IN SUB-DIRECTORY ---------------------------------------------------------
@@ -48,8 +50,10 @@ out <- filterAndTrim(file.path(path,fns), filts,
                      compress=TRUE, multithread=FALSE) # On Windows set multithread=FALSE
 
 head(out)
+#save output of filtering to RDS
 saveRDS(out, '3_filtered/filteredandtrimmed.RDS')
 
+#read output of filtering in (this allows you to avoid running filterAndTrim() again
 out<-readRDS('3_filtered/filteredandtrimmed.RDS')
 
 #subset three objects for only samples hat were actually filtered - see https://github.com/benjjneb/dada2/issues/1279
@@ -59,34 +63,52 @@ fns<-fns[file.exists(filts)]
 
 # LEARN THE ERROR RATE (takes a long time) ----------------------------------------------------
 
+#learn error rates
 err <- learnErrors(filts, multithread=FALSE)
+#save output of learning error rates i
 saveRDS(err, '4_dada2/err.RDS')
+#read output of learning error rates in (this allows you to avoid running learnErrors() again
+err<-readRDS('4_dada2/err.RDS')
 
 #hashed out cos takes ages and can crash pipeline
 plotErrors(err, nominalQ=TRUE)
 
 # SAMPLE INFERENCE --------------------------------------------------------
 
+#run DADA2's main sample/ASV inference function
 dada <- dada(filts, err=err, multithread=FALSE)
+#save output of sample inference
 saveRDS(dada,'4_dada2/dada_output.RDS')
+#read output of sample inference in (this allows you to avoid running dada() again
+dada<-readRDS('4_dada2/dada_output.RDS')
 
 # CONSTRUCT SEQUENCE TABLE ------------------------------------------------
 
+#construct sequence table
 seqtab <- makeSequenceTable(dada)
+#check dimensions to get number of samples and ASVs
 dim(seqtab)
+#save sequence table as RDS
 saveRDS(seqtab, '4_dada2/seqtab.RDS')
-seqtab<-readRDS('4_dada2/seqtab.RDS')
+#save sequence table as CSV for more human-readable format
 write.csv(seqtab,'4_dada2/seqtab.csv')
 
-table(nchar(getSequences(seqtab)))
+#read in sequence table (this allows you to avoid running makeSequenceTable again)
+seqtab<-readRDS('4_dada2/seqtab.RDS')
 
 # REMOVE CHIMERAS https://github.com/benjjneb/dada2/issues/1430 ---------------------------------------------------------
 
+#remove chimeras
 seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=FALSE, verbose=TRUE)
+#check dimensions to get number of samples and ASVs
 dim(seqtab.nochim)
+#save sequence table as RDS
 saveRDS(seqtab.nochim, '4_dada2/seqtab.nochim.RDS')
-seqtab.nochim<-readRDS('4_dada2/seqtab.nochim.RDS')
+#save sequence table as CSV for more human-readable format
 write.csv(seqtab.nochim,'4_dada2/seqtab.nochim.csv')
+
+#read in sequence table (this allows you to avoid running removeBimeraDenovo again)
+seqtab.nochim<-readRDS('4_dada2/seqtab.nochim.RDS')
 
 # TRACK READS THROUGH THE PIPELINE ----------------------------------------
 
@@ -99,15 +121,19 @@ head(track)
 
 # ASSIGN TAXONOMY ---------------------------------------------------------
 
+#assign taxonmy to ASVs using SILVA database
 taxa <- assignTaxonomy(seqtab.nochim, "4_dada2/silva_nr99_v138.1_train_set.fa.gz", multithread=FALSE)
+#save taxonomy table as RDS
 saveRDS(taxa, '4_dada2/taxa.RDS')
+#save taxonomy table as CSV for more human-readable format
 write.csv(taxa, '4_dada2/taxa.csv')
-#taxa<-readRDS('4_dada2/taxa.RDS')
+#read in taxonomy table (this allows you to avoid running assignTaxonomy() again
+taxa<-readRDS('4_dada2/taxa.RDS')
 
 # ASSIGN SPECIES-LEVEL TAXONOMY ------------------------------------------------------
 
-taxa<-readRDS('4_dada2/taxa.RDS')
-species_fasta="4_dada2/silva_species_assignment_v138.1.fa.gz"
+#read in species level SILVA database
+species_fasta<-"4_dada2/silva_species_assignment_v138.1.fa.gz"
 
 # There is some memory issue, causing the addSpecies to fail (https://github.com/benjjneb/dada2/issues/733)
 # So we cut the table in chunks:
@@ -121,38 +147,44 @@ chunks.species <- lapply(chunks,
                          })
 taxa_wsp <- do.call(rbind, chunks.species)
 
+#save species-level taxonomy table as RDS
 saveRDS(taxa_wsp, '4_dada2/taxa_wsp.RDS')
+#save species-level taxonomy table as CSV for more human-readable format
 write.csv(taxa_wsp, '4_dada2/taxa_wsp.csv')
 
-taxa.print <- taxa # Removing sequence rownames for display only
+# Removing sequence rownames for display only
+taxa.print <- taxa 
 rownames(taxa.print) <- NULL
+#inspect the output
 head(taxa.print)
-
 
 # WRITE METADATA ----------------------------------------------------------
 
-seqtab.nochim<-readRDS('4_dada2/seqtab.nochim.RDS')
+#get the final list of sample names for the samples after chimera removal (sample as lines 22-32)
 samples.out <- rownames(seqtab.nochim)
 samples.out <- gsub("\\.", "_", samples.out)
 samples.out <- sub("\\-.*", "", samples.out)
 
+#get the sampling days for each sample
 day <- sapply(strsplit(samples.out, "_"), `[`, 1)
 day <- as.numeric(gsub("day","",day))
+
+#get the 96 well plate well for each sample
 well <- sapply(strsplit(samples.out, "_"), `[`, 2)
+
+#get the community name for each sample
 community <- sapply(strsplit(samples.out, "_"), `[`, 3)
+
+#get the experimental replicate (1-4) for each sample
 rep <- as.numeric(sapply(strsplit(samples.out, "_"), `[`, 4))
+
+#get the sequence run ID for each sample
 seq.run<-c()
 seq.run[grep('052214DR16s', samples.out)]<-'052214DR16s'
 seq.run[grep('021216DR515F', samples.out)]<-'021216DR515F'
-levels(as.factor(seq.run))
+levels(as.factor(seq.run)) #check number of runs (should be 2)
 
-seq.run
-
+#build the metadata dataframe based on above
 samdf <- data.frame(community=community, day=day, rep=rep, seq.run=seq.run, well=well)
+#save it as a CSV
 write.csv(samdf,'4_dada2/samdf.csv')
-
-
-ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), 
-               sample_data(samdf), 
-               tax_table(taxa))
-ps <- prune_samples(sample_names(ps) != "Mock", ps) # Remove mock sample
